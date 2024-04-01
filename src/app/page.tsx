8bbfb437789e28ed37ecb84a097fbca19df5ab5f
ikatiemusgrave <165228121+ikatiemusgrave@users.noreply.github.com>
2024-04-01 20:05:42 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,13 @@ export default function HomePage() {
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
           <Logo className='w-16' />
-          <h1 className='mt-4'>Katie is king</h1>
+          <h1 className='mt-4'>Katie is King</h1>
           <p className='mt-2 text-sm text-gray-800'>
-            A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-            Import, Seo, Link component, pre-configured with Husky{' '}
+           I make text. 
           </p>
           <p className='mt-2 text-sm text-gray-700'>
-            <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-              See the repository
+            <ArrowLink href='https://katiemusgrave.com'>
+              Visit my website
             </ArrowLink>
           </p>
 
